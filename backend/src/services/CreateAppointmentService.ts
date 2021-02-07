74220884ff65bfd3295d256c9402a681eb325cd1
Henrique Monteiro <henrique.monteiro.martins@hotmail.com>
2021-02-07 19:08:34 -0300

    BACKEND -> DB e criação da Tabela agendamento

--- backend/src/services/CreateAppointmentService.ts
+++ backend/src/services/CreateAppointmentService.ts
@@ -1,32 +1,21 @@
 import { startOfHour } from 'date-fns';
+import { getCustomRepository } from 'typeorm';
 
 import Appointment from '../models/Appointment';
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
-import appointmentsRouter from '../routes/appointments.routes';
 
 interface Request {
   provider: string;
   date: Date;
 }
 
-/**
- * TODO:
- * [X] Recebimento das informações
- * [X] Tratativas de erros FIXME:
- * [X] Acesso ao repositório
- */
-
 class CreateAppointmentService {
-  private appointmentsRepository: AppointmentsRepository;
+  public async execute({ provider, date }: Request): Promise<Appointment> {
+    const appointmentsRepository = getCustomRepository(AppointmentsRepository);
 
-  constructor(appointmentsRepository: AppointmentsRepository) {
-    this.appointmentsRepository = appointmentsRepository;
-  }
-
-  public execute({ provider, date }: Request): Appointment {
     const appointmentDate = startOfHour(date);
 
-    const findAppointmentInSameDate = this.appointmentsRepository.findByDate(
+    const findAppointmentInSameDate = await appointmentsRepository.findByDate(
       appointmentDate,
     );
 
@@ -34,10 +23,12 @@ class CreateAppointmentService {
       throw Error('This appointment is alredy booked');
     }
 
-    const appointment = this.appointmentsRepository.create({
+    const appointment = appointmentsRepository.create({
       provider,
       date: appointmentDate,
     });
+
+    await appointmentsRepository.save(appointment);
 
     return appointment;
   }
