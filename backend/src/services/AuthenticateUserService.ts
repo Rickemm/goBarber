e399417f1dff51baa45f706cabb8b3982a18fa4f
Henrique Monteiro <henrique.monteiro.martins@hotmail.com>
2021-02-17 21:32:45 -0300

    BACKEND -> Tratativas de erros implementada

--- backend/src/services/AuthenticateUserService.ts
+++ backend/src/services/AuthenticateUserService.ts
@@ -1,10 +1,10 @@
 import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
 import { sign } from 'jsonwebtoken';
-import authConfig from '../config/auth';
 
+import authConfig from '../config/auth';
 import User from '../models/User';
-import usersRouter from '../routes/users.routes';
+import AppError from '../errors/AppError';
 
 interface Request {
   email: string;
@@ -23,13 +23,13 @@ class AuthenticateUserService {
     const user = await userRepository.findOne({ where: { email } });
 
     if (!user) {
-      throw new Error('Incorrect email/password combination.');
+      throw new AppError('Incorrect email/password combination.', 401);
     }
 
     const passwordMatched = await compare(password, user.password);
 
     if (!passwordMatched) {
-      throw new Error('Incorrect email/password combination.');
+      throw new AppError('Incorrect email/password combination.', 401);
     }
 
     const { secret, expiresIn } = authConfig.jwt;
